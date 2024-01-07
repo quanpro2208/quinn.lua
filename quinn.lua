6450f389f370e2fed45ffa32db58b3afd5334c0f
quanpro2208 <92915634+quanpro2208@users.noreply.github.com>
2024-01-07 20:07:24 +0700

    Update quinn.lua

--- quinn.lua
+++ quinn.lua
@@ -1,119 +1,81 @@
-repeat wait()
-until game:IsLoaded()
-local TableChat = {"siuuu"}
-spawn(function()
-    while wait() do 
-        pcall(function()
-            game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(TableChat[math.random(1,#TableChat)],"All")
-            wait(45)
-        end)
-    end
-end)
-getgenv().Setting = {
-    ["Team"] = "Pirates", --Marines,Pirates
-    ["Webhook"] = {
-        ["Enabled"] = true,
-        ["Url Webhook"] = "https://discord.com/api/webhooks/1190860668451180675/FQSMYFdezZ-GvxfYXbUlNyIH4qxIE9_2oHF4B5jePUT74MpNPdBI1_vgxS0UDnBPukqM", --Your Url
-    },
-    ["Misc"] = {
-        ["AutoBuyRandomandStoreFruit"] = false,
-        ["AutoBuySurprise"] = false,
-    },
-    ["Click"] = {
-        ["Enable"] = true,
-        ["Click Gun"] = true,
-        ["OnLowHealthDisable"] = true,
-        ["LowHealth"] = 4500,
-    },
-    ["SafeZone"] = {
-        ["Enable"] = true,
-        ["LowHealth"] = 3000,
-        ["MaxHealth"] = 5000,
-        ["Teleport Y"] = math.random(1000,2000)
-    },
-    ["Race V4"] = {
-        ["Enable"] = true,
-    },
-    ["Invisible"] = true,
-    ["White Screen"] = true,
-    ["GunMethod"] = false, --Support Only Melee And Gun,Not Invisible, Turn On Enabled Gun and Melee Please
-    ["SpamSkill"] = false, -- Will use all skills as fast as possbile ignore holding skills
-    ["Weapons"] = {
-        ["Melee"] = {
-            ["Enable"] = true,
-            ["Delay"] = 3,
-            ["Skills"] = {
-                ["Z"] = {
-                    ["Enable"] = true,
-                    ["HoldTime"] = 1.5,
-                },
-               [ "X"] = {
-                    ["Enable"] = true,
-                    ["HoldTime"] = 0,
-                },
+_G.Team = "Pirates"
+_G.FpsBoost = false
+_G.Webhook = ""
 
-                ["C"] = {
-                    ["Enable"] = true,
-                    ["HoldTime"] = 0.2,
-                },
-            },
-        },
-        ["Blox Fruit"] = {
-            ["Enable"] = true,
-            ["Delay"] = 1,
-            ["Skills"] = {
-                ["Z"] = {
-                    ["Enable"] = false,
-                    ["HoldTime"] = 0,
-                },
-                ["X"] = {
-                    ["Enable"] = true,
-                    ["HoldTime"] = 0.23,
-                },
-
-                ["C"] = {
-                    ["Enable"] = true,
-                    ["HoldTime"] = 2,
-                },
-                ["V"] = {
-                    ["Enable"] = false,
-                    ["HoldTime"] = 0,
-                },
-                ["F"] = {
-                    ["Enable"] = false,
-                    ["HoldTime"] = 0,
-                },
-            },
-        },
-        ["Gun"] = {
-            ["Enable"] = false,
-            ["Delay"] = 2,
-            ["Skills"] = {
-                ["Z"] = {
-                    ["Enable"] = false,
-                    ["HoldTime"] = 0.2,
-                },
-                ["X"] = {
-                    ["Enable"] = true,
-                    ["HoldTime"] = 0.001,
-                },
-            },
-        },
-        ["Sword"] = {
-            ["Enable"] = true,
-            ["Delay"] = 1,
-            ["Skills"] = {
-                ["Z"] = {
-                    ["Enable"] = true,
-                    ["HoldTime"] = 1.5,
-                },
-                ["X"] = {
-                    ["Enable"] = true,
-                    ["HoldTime"] = 0.3,
-                },
-            },
-        },
+getgenv().Chatkill = {
+    ""
+}
+getgenv().Region_Hop = {
+    ["Enable"] = true, -- true/false
+    ["Region"] = "Singapore"
+    --[[Singapore, United States, Germany, Japan, France, Australia, etc
+        - Singapore is the best choice for Asia
+    ]]
+}
+_G.Setting = {
+    ["Start Hunting"] = true, -- Bro wtf don't turn it off
+    ["Ken Haki"] = true,
+    ["WhiteScreen"] = false,
+    ["FruitStuff"] = false, -- Auto Buy and Store Fruits
+    ["Run"] = 4500, -- As the name, you'll run when the health below ...
+    ["MaxHealth"] = 5000,
+    ["Time&Bounty Counter"] = true,
+    ["Click Delay"] = 0.2,
+    ["SkipPlayerWhenBeingLowHealth"] = false,
+    ["Check"] = {
+        ["V4 Players"] = false,
+        ["Portal Users"] = true,
+        ["Buddha Users"] = false
+    },
+    ["Theme"] = { -- You can get RGB colors from "Color Picker" on google
+        ["Default"] = "YaeMiko", -- Use this if you don't know how to customize the theme, get themes on: https://github.com/clgtmm/ThemeBuilds
+        ["Custom Theme"] = {
+            ["Enable"] = true, -- Set this "false" if not knowing how to customize the theme
+            ["Color"] = {
+                ["Title Color"] = Color3.fromRGB(0, 170, 255),
+                ["Title-Back Color"] = Color3.fromRGB(0, 85, 255),
+                ["Button Color"] = Color3.fromRGB(0, 0, 255),
+                ["Stats Color"] = {
+                    ["Text Color"] = Color3.fromRGB(0, 170, 255),
+                    ["Text Stroke"] = Color3.fromRGB(30, 56, 203)
+                }
+            }
+        }
+    },
+    ["Melee"] = {
+        ["Time"] = 1,
+        ["Enable"] = true,
+        ["Z"] = {["Enable"] = true, ["HoldTime"] = 0.75},
+        ["X"] = {["Enable"] = true, ["HoldTime"] = 0.1},
+        ["C"] = {["Enable"] = true, ["HoldTime"] = 1}
+    },
+    ["Fruit"] = {
+        ["Time"] = 1,
+        ["Enable"] = true,
+        ["Z"] = {["Enable"] = true, ["HoldTime"] = 1.75},
+        ["X"] = {["Enable"] = true, ["HoldTime"] = 0},
+        ["C"] = {["Enable"] = true, ["HoldTime"] = 0},
+        ["V"] = {["Enable"] = false, ["HoldTime"] = 0},
+        ["F"] = {["Enable"] = true, ["HoldTime"] = 0}
+    },
+    ["Sword"] = {
+        ["Time"] = 0.15,
+        ["Enable"] = true,
+        ["Z"] = {["Enable"] = true, ["HoldTime"] = 0.1},
+        ["X"] = {["Enable"] = false, ["HoldTime"] = 0},
+    },
+    ["Gun"] = {
+        ["Time"] = 0.8,
+        ["Enable"] = true,
+        ["GunMode"] = true,
+        ["Z"] = {["Enable"] = true, ["HoldTime"] = 0},
+        ["X"] = {["Enable"] = true, ["HoldTime"] = 0},
     }
 }
-repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
-loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BountyShit.lua"))()
+delay(360,function()
+    if not game.Workspace:FindFirstChild("HiHiHi") then
+      game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.LocalPlayer)
+    end
+end)
+repeat wait() until game:IsLoaded()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/clgtmm/AutoBounty/main/main.lua"))()
